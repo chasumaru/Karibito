fd6e0fb1618d9a9bed34b4df90c0dd25dd55c0dd
chasumarudesu <chasumarudesu@gmail.com>
2022-05-10 19:30:04 +0900

    Add devise #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users,
+      module: "users",
+      path: '',
+      path_names: {
+      sign_in: 'login', sign_out: 'logout',
+      password: 'secret', confirmation: 'verification',
+      registration: 'register', edit: 'edit/profile'
+    }
+  
+  root 'pages#index'
+  get "pages/show"
   get 'testpages/index'
   get 'testpages/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
