0bd885a70fbb0c7f8115076fbbce12799b696d50
chasumarudesu <chasumarudesu@gmail.com>
2022-05-15 13:49:54 +0900

    modify column username to name

--- db/migrate/20220510085932_add_columns_to_users.rb
+++ db/migrate/20220510085932_add_columns_to_users.rb
@@ -2,6 +2,6 @@ class AddColumnsToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
-    add_column :users, :username, :string
+    add_column :users, :name, :string
   end
 end
