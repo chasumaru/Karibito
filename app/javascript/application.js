4419a13c61cbdd77ff3818c8a9699d3fc49c23aa
chasumarudesu <chasumarudesu@gmail.com>
2022-05-17 20:12:16 +0900

    confirmation dialog logout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
+// サンプル
 import Vue from 'vue'
 var vm = new Vue({
   el: '#example',
@@ -7,3 +8,5 @@ var vm = new Vue({
     domain: 'HAZM.JP'
   }
 })
+
+import "@hotwired/turbo-rails"
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,4 @@
 
 pin "application", preload: true
 pin "vue", to: "https://unpkg.com/vue@2.6.11/dist/vue.esm.browser.min.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
