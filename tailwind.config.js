79e5abf1cd4b473f0b9e519e65e4305b4da2457f
chasumarudesu <chasumarudesu@gmail.com>
2022-05-18 02:31:06 +0900

    How to use pin, package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  darkMode:'media ',
+  theme: {
+    extend: {
+    darkMode: 'media',
+      colors: {
+        'teal': colors.teal,
+        hobby: {
+          100: '#001871',
+          200: '#ff585d',
+          300: '#ffb549',
+          400: '#41b6e6',
+        }
+      },
+    },
+  },
 }
