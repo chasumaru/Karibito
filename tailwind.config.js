063640d82cd495c02f00f3c6bcdb22f1c69819d4
chasumarudesu <chasumarudesu@gmail.com>
2022-05-23 02:42:44 +0900

    not round form

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,17 +11,20 @@ module.exports = {
   darkMode:'media ',
   theme: {
     screens: {
-      'sm': '520px',
-      // => @media (min-width: 520px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
 
       'md': '768px',
       // => @media (min-width: 768px) { ... }
 
-      'lg': '960px',
-      // => @media (min-width: 960px) { ... }
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
 
       'xl': '1280px',
       // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
     extend: {
       colors: {
