8a74a1fe21469ad152c122e6dfedcf81cc0b0439
chasumarudesu <chasumarudesu@gmail.com>
2022-06-05 19:59:28 +0900

    if you want to use turbo you should change data-turbo=false to data-turbo=true

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,7 @@
 // Hotwireを適切に設定しないと、エラーメッセージが表示されなくなる
 import "@hotwired/turbo-rails"
 // Hotwireを利用したくない場面では適宜無効化する
-Turbo.session.drive = false
+// Turbo.session.drive = false
 
 
 // import Vue from 'vue'
