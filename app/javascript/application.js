85f73e52ecd651941825a78ef0d384d6fbcfe250
chasumarudesu <chasumarudesu@gmail.com>
2022-06-09 04:05:57 +0900

    make partials

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -13,7 +13,7 @@ function hamburger() {
   document.getElementById('line1').classList.toggle('line_1');
   document.getElementById('line2').classList.toggle('line_2');
   document.getElementById('line3').classList.toggle('line_3');
-  document.getElementById('nav').classList.toggle('in');
+  document.getElementById('sidebar').classList.toggle('in');
 }
 document.getElementById('hamburger').addEventListener('click' , function () {
   hamburger();
