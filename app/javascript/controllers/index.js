777c065e3bccad5921872e7497ddc13e2026d82c
chasumarudesu <chasumarudesu@gmail.com>
2022-07-15 15:22:07 +0900

    set tailwind-stimulus-components

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,20 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+// tailwindcss-stimulus-componentsの追加
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+application.register('alert', Alert)
+application.register('autosave', Autosave)
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
+application.register('popover', Popover)
+application.register('toggle', Toggle)
+application.register('slideover', Slideover)
+
+// import TextareaAutogrow from "stimulus-textarea-autogrow"
+// application.register("textarea-autogrow", TextareaAutogrow)
+
+// import ScrollTo from "stimulus-scroll-to"
+// application.register("scroll-to", ScrollTo)
