532f1b0625e152c9652c10410cd7bef284355f21
chasumarudesu <chasumarudesu@gmail.com>
2022-08-07 17:11:00 +0900

    image_processingの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,15 @@ class ApplicationController < ActionController::Base
 
   include Pagy::Backend
 
-  unless Rails.env.development?
-    rescue_from Exception,                      with: :_render_500
-    rescue_from ActiveRecord::RecordNotFound,   with: :_render_404
-    rescue_from ActionController::RoutingError, with: :_render_404
-  end
+  # unless Rails.env.development?
+  #   rescue_from Exception,                      with: :_render_500
+  #   rescue_from ActiveRecord::RecordNotFound,   with: :_render_404
+  #   rescue_from ActionController::RoutingError, with: :_render_404
+  # end
   
-  def routing_error
-    raise ActionController::RoutingError, params[:path]
-  end
+  # def routing_error
+  #   raise ActionController::RoutingError, params[:path]
+  # end
 
   def after_sign_in_path_for(resource)
     mypage_path(resource)
@@ -21,24 +21,24 @@ class ApplicationController < ActionController::Base
   
   private
   
-  def _render_404(e = nil)
-    logger.info "Rendering 404 with excaption: #{e.message}" if e
-    if request.format.to_sym == :json
-      render json: { error: "404 Not Found" }, status: :not_found
-    else
-      render "errors/404.html", status: :not_found, layout: "error"
-    end
-  end
+  # def _render_404(e = nil)
+  #   logger.info "Rendering 404 with excaption: #{e.message}" if e
+  #   if request.format.to_sym == :json
+  #     render json: { error: "404 Not Found" }, status: :not_found
+  #   else
+  #     render "errors/404.html", status: :not_found, layout: "error"
+  #   end
+  # end
 
-  def _render_500(e = nil)
-    logger.error "Rendering 500 with excaption: #{e.message}" if e
+  # def _render_500(e = nil)
+  #   logger.error "Rendering 500 with excaption: #{e.message}" if e
 
-    if request.format.to_sym == :json
-      render json: { error: "500 Internal Server Error" }, status: :internal_server_error
-    else
-      render "errors/500.html", status: :internal_server_error, layout: "error"
-    end
-  end
+  #   if request.format.to_sym == :json
+  #     render json: { error: "500 Internal Server Error" }, status: :internal_server_error
+  #   else
+  #     render "errors/500.html", status: :internal_server_error, layout: "error"
+  #   end
+  # end
 
   def sign_in_required
     redirect_to new_user_session_url unless user_signed_in?
