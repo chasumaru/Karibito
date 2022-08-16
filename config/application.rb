2f61676845d31545531693253b9edc3ef6c9896b
chasumarudesu <chasumarudesu@gmail.com>
2022-08-16 16:48:55 +0900

    投稿ページのレイアウト

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     #ActiveStorage上書きしない
     config.active_storage.replace_on_assign_to_many = false
