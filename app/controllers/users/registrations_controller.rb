689d425d7abc7f292c19be7fe1ef16e4d9caafb7
chasumarudesu <chasumarudesu@gmail.com>
2022-08-27 04:01:42 +0900

    プロフィール編集画面の完成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
   before_action :sign_in_required, only: [:edit, :update, :destroy]
 
   # GET /resource/sign_up
