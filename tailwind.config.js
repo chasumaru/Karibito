23d64f4126b64f1d1e7a4bc840565d09d88f0093
chasumarudesu <chasumarudesu@gmail.com>
2022-08-27 20:26:07 +0900

    画面のfade in を設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,15 @@ module.exports = {
       ]
     },
     extend: {
+      animation: {
+        "fade-in": "fadeIn 0.2s ease-in-out"
+      },
+      keyframes: () => ({
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        }
+      }),
       colors: {
         'base-white': '#f9fafb',
         'base-gray' : '#f4f4f5',
