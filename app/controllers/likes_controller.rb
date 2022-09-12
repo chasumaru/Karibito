adf7256bd787eb4d6c9370b467a937592b164852
chasumarudesu <chasumarudesu@gmail.com>
2022-09-13 05:19:42 +0900

    非同期いいね数、コメント数

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,28 +6,12 @@ class LikesController < ApplicationController
     @like = current_user.likes.create(post_id: params[:post_id])
     @liked_users = @post.liked_users
     @post.create_notification_by(current_user)
-    flash.now.notice = "いいねができまsita。" 
-    # redirect_to request.referer, status: :see_other 
   end
-
-  # def destroy
-  #   @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
-  #   @like.destroy
-  #   redirect_to request.referer, status: :see_other 
-  # end
-  
-  # def create
-  #   @like = current_user.likes.create(post_id: params[:post_id])
-  #   if @like.save
-  #     @post.create_notification_by(current_user)
-  #     flash.now.notice = "いいねしました。" 
-  #   end
-  # end
 
   def destroy
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    flash.now.notice = "いいねを取り消しました。"
+    @liked_users = @post.liked_users
   end
   
   private
@@ -37,5 +21,9 @@ class LikesController < ApplicationController
 
     def set_post
       @post = Post.find(params[:post_id])
+    end
+    
+    def set_liked_users
+      @liked_users = @post.liked_users
     end
 end
