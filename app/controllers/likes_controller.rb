4d23c23242b35d36ec4720c614064f70370519b6
chasumarudesu <chasumarudesu@gmail.com>
2022-09-12 22:57:42 +0900

    likeのアイコンを分ける

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,28 @@
 class LikesController < ApplicationController
   before_action :sign_in_required, {only: [:create, :destroy]}
+  before_action :set_post, {only: [:create, :destroy]}
 
   def create
     @like = current_user.likes.create(post_id: params[:post_id])
-    @post = Post.find(params[:post_id])
     @post.create_notification_by(current_user)
-    redirect_back(fallback_location: root_path)
+    flash.now.notice = "いいねができまsita。" if @post.save
+    unless @post.save
+      flash.now.notice = "いいねができませんでした。"
+    end
   end
 
   def destroy
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    redirect_to request.referer, status: :see_other 
+    flash.now.notice = "いいねができませんでした。"
   end
   
   private
     def like_params
       params.require(:like).permit(:post_id, :user_id)
+    end
+
+    def set_post
+      @post = Post.find(params[:post_id])
     end
 end
