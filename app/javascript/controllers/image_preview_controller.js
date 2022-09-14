4c7a730ede0cb02683e3bc60397421200e5d8dfe
chasumarudesu <chasumarudesu@gmail.com>
2022-09-14 12:30:51 +0900

    z-indexはaccounts/showに設定しない

--- app/javascript/controllers/image_preview_controller.js
+++ app/javascript/controllers/image_preview_controller.js
@@ -3,22 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "output", "input", "bgOutput", "bgInput" ]
 
-//   readURL() {
-//     if (this.inputTarget){
-//     var input = this.inputTarget
-//     var output = this.outputTarget}
-//     if (this.bgOutputTarget){
-//     var input = this.inputTarget
-//     var output = this.outputTarget}
-//     if (input.files && input.files[0]) {
-//       var reader = new FileReader();
-
-//       reader.onload = function () {
-//        output.src = reader.result
-//      }
-//      reader.readAsDataURL(input.files[0]);
-//    }
-//  }
 
  readURL() {
   var input = this.inputTarget
