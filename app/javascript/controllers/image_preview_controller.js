5c4c645be6913ba3cf596acbfa9a0acabb51f583
chasumarudesu <chasumarudesu@gmail.com>
2022-09-14 11:21:19 +0900

    プロフィール編集ページのプレビュー機能

--- app/javascript/controllers/image_preview_controller.js
+++ app/javascript/controllers/image_preview_controller.js
@@ -1,21 +1,46 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "output", "input" ]
+  static targets = [ "output", "input", "bgOutput", "bgInput" ]
 
-  readURL() {
-    var input = this.inputTarget
-    var output = this.outputTarget
+//   readURL() {
+//     if (this.inputTarget){
+//     var input = this.inputTarget
+//     var output = this.outputTarget}
+//     if (this.bgOutputTarget){
+//     var input = this.inputTarget
+//     var output = this.outputTarget}
+//     if (input.files && input.files[0]) {
+//       var reader = new FileReader();
 
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
+//       reader.onload = function () {
+//        output.src = reader.result
+//      }
+//      reader.readAsDataURL(input.files[0]);
+//    }
+//  }
 
-      reader.onload = function () {
-       output.src = reader.result
-     }
-
-     reader.readAsDataURL(input.files[0]);
+ readURL() {
+  var input = this.inputTarget
+  var output = this.outputTarget
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function () {
+     output.src = reader.result
    }
+   reader.readAsDataURL(input.files[0]);
  }
+}
+readBgURL() {
+  var input = this.bgInputTarget
+  var output = this.bgOutputTarget
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function () {
+     output.src = reader.result
+   }
+   reader.readAsDataURL(input.files[0]);
+ }
+}
 
 }
