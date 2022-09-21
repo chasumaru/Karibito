359ad31a071a47056ff66cf3986112f7cbd1c0fc
chasumarudesu <chasumarudesu@gmail.com>
2022-09-21 10:46:18 +0900

    static_pages/index 日記一覧の土台が完成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,13 @@
 class StaticPagesController < ApplicationController
 
+  def index
+    @posts = Post.all
+    @users = User.all
+  end
+
   def about; end
   def privacy; end
   def faq; end
-  def index; end
+
 end
 
