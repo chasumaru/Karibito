b82b8e8485cd6683303c700945b1f6e97e62bda6
chasumarudesu <chasumarudesu@gmail.com>
2022-10-05 20:53:01 +0900

    loading画像を消し、post,boardのパーシャル化

--- app/models/user.rb
+++ app/models/user.rb
@@ -75,38 +75,38 @@ class User < ApplicationRecord
     end
   end
 
-  def self.from_omniauth(auth)
-    find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-    end
-  end
+  # def self.from_omniauth(auth)
+  #   find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
+  #     user.provider = auth.provider
+  #     user.uid = auth.uid
+  #     user.name = auth.info.name
+  #   end
+  # end
   
-  def self.new_with_session(params, session)
-    if session["devise.user_attributes"]
-      new(session["devise.user_attributes"]) do |user|
-        user.attributes = params
-      end
-    else
-      super
-    end
-  end
+  # def self.new_with_session(params, session)
+  #   if session["devise.user_attributes"]
+  #     new(session["devise.user_attributes"]) do |user|
+  #       user.attributes = params
+  #     end
+  #   else
+  #     super
+  #   end
+  # end
 
-  def self.find_for_oauth(auth)
-    user = User.where(uid: auth.uid, provider: auth.provider).first
+  # def self.find_for_oauth(auth)
+  #   user = User.where(uid: auth.uid, provider: auth.provider).first
 
-    unless user
-      user = User.create(
-        uid:      auth.uid,
-        provider: auth.provider,
-        email:    User.dummy_email(auth),
-        password: Devise.friendly_token[0, 20]
-      )
-    end
-    user.skip_confirmation! #仮登録メールを介さずに即時登録
-    user
-  end
+  #   unless user
+  #     user = User.create(
+  #       uid:      auth.uid,
+  #       provider: auth.provider,
+  #       email:    User.dummy_email(auth),
+  #       password: Devise.friendly_token[0, 20]
+  #     )
+  #   end
+  #   user.skip_confirmation! #仮登録メールを介さずに即時登録
+  #   user
+  # end
 
 
   private
