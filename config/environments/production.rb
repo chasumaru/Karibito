1042af35fcf998b4eeb9dbf9a4388e05c323451e
chasumarudesu <chasumarudesu@gmail.com>
2022-11-03 17:51:52 +0900

    mailer設定の変更1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
     address:"smtp.gmail.com",
     domain: 'gmail.com',
     port:587,
-    user_name: ENV['SEND_MAIL'],
-    password: ENV['GMAIL_SPECIFIC_PASSWORD'],
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
     authentication: :login,
     openssl_verify_mode: 'none',
     enable_starttls_auto: true
