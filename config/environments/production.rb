44d37b6a07952dcabb61999a8a981fb5a6de526f
chasumarudesu <chasumarudesu@gmail.com>
2022-11-03 18:37:31 +0900

    production.rbのハッシュ記法を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,11 +71,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
    :address => 'smtp.gmail.com',
-   :domain: 'gmail.com',
+   :domain => 'gmail.com',
    :port => 587,
    :user_name => Rails.application.credentials.gmail[:user_name],
    :password => Rails.application.credentials.gmail[:password],
-    :authentication => :login,
+   :authentication => :login,
     # :authentication => :plain,
    :openssl_verify_mode => 'none',
    :enable_starttls_auto => true
